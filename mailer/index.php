f9146758029c265a2c48c66e9df1337ad872aed8
Skipper Strange <skipperstrange@gmail.com>
2021-04-19 01:31:45 +0000

    Ajax form communications between front end and backend established.

--- mailer/index.php
+++ mailer/index.php
@@ -3,16 +3,22 @@ ini_set('display_error', 1);
 include './includes/const.php';
 include './includes/fxns.php';
 include './includes/db/mailerModel.php';
+
+include 'vendor/autoload.php';
+
 @$action = empty($_GET['action']) ? 'default' : $_GET['action'];
 
 
 switch($action){
     case "sendmsg":
+
+        if($_POST){
+            $info = $_POST;
+            print_r($info);
+        }
         
         if(IS_AJAX){
-            echo "Ajax triggered";
-
-            return json_response("Message sent");
+            echo json_response("Message sent");
         }
 
        
