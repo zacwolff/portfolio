02d049338d94271046a6e7ec24deaf683e552d2c
Skipper Strange <skipperstrange@gmail.com>
2021-04-19 02:08:12 +0000

    Server scripts added. Testing to be done before proceeding.

---
+++ mailer/includes/settings.php
@@ -0,0 +1,11 @@
+<?php
+$config = 
+    //mailer config 
+    [
+        'mailer'=>[
+            'host' => '',
+            'from' => 'studio@zacwolff.com',
+            'name' => 'Zac Wolff',
+            'reply' => 'studio@zacwolff.com'
+        ]
+     ];
--- mailer/index.php
+++ mailer/index.php
@@ -2,7 +2,18 @@
 ini_set('display_error', 1);
 include './includes/const.php';
 include './includes/fxns.php';
+include './includes/settings.php';
+
+require './vendor/phpmailer/phpmailer/src/Exception.php';
+require './vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require './vendor/phpmailer/phpmailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 include './includes/db/mailerModel.php';
+
+
 
 include 'vendor/autoload.php';
 
@@ -15,10 +26,36 @@ switch($action){
         if($_POST){
             $info = $_POST;
             print_r($info);
+            print_r($config['mailer']);
+
+            //Setting Up PHPMailer
+            $mail = new PHPMailer();
+            $mail->isSMTP();
+            $mail->Host = 'smtp.outlook.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = ''; 
+            $mail->Password = ''; 
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 2525;
+            $mail->isHTML(true);
+
+            $mail->setFrom($config['mailer']['from'], $config['mailer']['name']);
+            $mail->addReplyTo($config['mailer']['reply'], $config['mailer']['name']);
+            $mail->addAddress($info['email']);
+            $mail->Subject = $info['subject'];
+            $mail->Body = $info['message'];
+
+            if($mail->send()){
+                echo 'Message has been sent';
+            }else{
+                echo 'Message could not be sent.';
+                echo 'Mailer Error: ' . $mail->ErrorInfo;
+            }
+            
         }
         
         if(IS_AJAX){
-            echo json_response("Message sent");
+            return json_response("Message sent");
         }
 
        
